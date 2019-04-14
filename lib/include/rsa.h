326a645cd23a1a010d111189c8ff45385702875d
Dima Lukashov <lds4ever2000@gmail.com>
2019-04-14 22:14:55 +0300

    Change library logic

--- lib/include/rsa.h
+++ lib/include/rsa.h
@@ -1,23 +1,36 @@
 #ifndef _LIBRSA_H
 #define _LIBRSA_H
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef uint16_t __key_t_;
 
 typedef struct key_pairs {
     __key_t_ public_key;
     __key_t_ private_key;
+    uint16_t base;
 } key_pair;
 
 const uint8_t FIRST_FOUR_BITS_MASK = 0x0F;
 
 
-inline int is_prime(uint8_t number);
+int is_prime(uint8_t number);
 void get_two_different_prime_numbers(uint8_t *buf);
-inline uint16_t get_augment(uint8_t first_prime, uint8_t second_prime);
-inline uint16_t get_euler_number(uint8_t first_prime, uint8_t second_prime);
-inline int gcd(uint16_t first_num, uint16_t second_num);
-inline void generate_key_pair(uint16_t euler_number, key_pair *kp);
-char* encrypt(char* message, key_pair *kp);
-char* decrypt(char* encrypted_message, key_pair* kp);
+void get_augment(uint8_t first_prime, uint8_t second_prime, key_pair *kp);
+uint16_t get_euler_number(uint8_t first_prime, uint8_t second_prime);
+int gcd(uint16_t first_num, uint16_t second_num);
+void generate_key_pair(key_pair *kp);
+void encrypt(
+        void *original_raw_data,
+        void *buffer,
+        uint32_t length,
+        key_pair *kp
+        );
+void decrypt(
+        void *original_encrypted_data,
+        void *buffer,
+        size_t size_of_raw_data,
+        uint16_t chunks,
+        key_pair *kp
+        );
 #endif
--- lib/rsa.c
+++ lib/rsa.c
@@ -1,12 +1,14 @@
 #include "./include/rsa.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/random.h>
+#include <math.h>
 
 
-extern inline int is_prime(uint8_t number) {
+int is_prime(uint8_t number) {
     int flag = 1;
-    for(int i=2; i < number/2; i++) {
+    for(int i=2; i < number; i++) {
         if(number % i == 0) {
             flag = 0;
             return flag;
@@ -23,69 +25,87 @@ void get_two_differenet_prime_numbers(uint8_t *buf) {
     while(1) {
         getentropy(&first_prime, sizeof(uint8_t));
         first_prime &= FIRST_FOUR_BITS_MASK;
-        if(first_prime > 1 && is_prime(first_prime)) break;
+        if(first_prime > 3 && is_prime(first_prime)) break;
     }
     buf[0] = first_prime;
 
     while(1) {
         getentropy(&second_prime, sizeof(uint8_t));
         second_prime &= FIRST_FOUR_BITS_MASK;
-        if(second_prime > 1 && is_prime(second_prime) && second_prime != first_prime) break;
+        if(second_prime > 5 && is_prime(second_prime) && second_prime != first_prime) break;
     }
     buf[1] = second_prime;
 }
 
 
-extern inline uint16_t get_augment(uint8_t first_prime, uint8_t second_prime) {
+void get_augment(uint8_t first_prime, uint8_t second_prime, key_pair *kp) {
     uint8_t fp = (uint16_t)first_prime;
     uint8_t sp = (uint16_t)second_prime;
-    return fp * sp;
+    kp->base = fp * sp;
 }
 
 
-extern inline uint16_t get_euler_number(uint8_t first_prime, uint8_t second_prime) {
+uint16_t get_euler_number(uint8_t first_prime, uint8_t second_prime) {
     uint16_t fp = (uint16_t)(first_prime - 1);
     uint16_t sp = (uint16_t)(second_prime - 1);
     return fp * sp;
 }
 
 
-extern inline int gcd(uint16_t first_num, uint16_t second_num) {
+int gcd(uint16_t first_num, uint16_t second_num) {
     if(second_num == 0) return first_num;
     return gcd(second_num, first_num % second_num);
 }
 
 
-extern inline void generate_key_pair(uint16_t euler_number, key_pair *kp) {
+void generate_key_pair(key_pair *kp) {
+    uint8_t* numbers = (uint8_t*)malloc(2*sizeof(uint8_t));
+    get_two_differenet_prime_numbers(numbers);
+    get_augment(numbers[0], numbers[1], kp);
+    uint16_t euler_number = get_euler_number(numbers[0], numbers[1]);
+    kp->public_key = 1;
     do {
-        while(1) {
-            getentropy(&(kp->public_key), sizeof(kp->public_key));
-            if(kp->public_key < euler_number && gcd(euler_number, kp->public_key) == 1) break;
+        kp->public_key += 1;
+        while(kp->public_key < euler_number) {
+            if(gcd(euler_number, kp->public_key) == 1) break;
+            kp->public_key += 1;
+        }
+        if(kp->public_key == euler_number) {
+            get_two_differenet_prime_numbers(numbers);
+            get_augment(numbers[0], numbers[1], kp);
+            euler_number = get_euler_number(numbers[0], numbers[1]);
+            kp->public_key = 1;
+            kp->private_key = 0;
+            continue;
         }
         kp->private_key = (__key_t_)((1 + euler_number) / kp->public_key);
+        if(kp->private_key == 1) continue;
     } while((kp->public_key * kp->private_key) % euler_number != 1);
+    free(numbers);
 }
 
 
+
+
+
 int main() {
-    uint8_t* numbers = (uint8_t*)malloc(2*sizeof(uint8_t));
     key_pair *kp = (key_pair*)malloc(sizeof(key_pair));
+    char *data = "Test";
+    char *encrypted = (char*)malloc(2*strlen(data));
+    char *decrypted = (char*)malloc(strlen(data));
 
-    get_two_differenet_prime_numbers(numbers);
-    printf("Number 1: %u\n", numbers[0]);
-    printf("Number 2: %u\n", numbers[1]);
+    generate_key_pair(kp);
 
-    uint16_t augment = get_augment(numbers[0], numbers[1]);
-    printf("Augment: %u\n", augment);
-
-    uint8_t euler_number = get_euler_number(numbers[0], numbers[1]);
-    printf("Euler number: %u\n", euler_number);
-
-    generate_key_pair(euler_number, kp);
     printf("Public key: %u\n", kp->public_key);
     printf("Private key: %u\n", kp->private_key);
 
-    free(numbers);
+
+    printf("Original: '%s'\n", data);
+    printf("Encrypted: '%s'\n", encrypted);
+    printf("Decrypted: '%s'\n", decrypted);
+
     free(kp);
+    free(encrypted);
+    free(decrypted);
     return 0;
 }
